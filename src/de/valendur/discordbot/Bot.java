f91f12afc4fe1d23c7f8bafcb020dfd57e287b41
FlayInAHook <timmae2001@gmail.com>
2020-12-06 15:39:48 +0100

    added basic structure

---
+++ src/de/valendur/discordbot/Bot.java
@@ -0,0 +1,5 @@
+package de.valendur.discordbot;
+
+public class Bot {
+	
+}
---
+++ src/de/valendur/discordbot/Main.java
@@ -0,0 +1,9 @@
+package de.valendur.discordbot;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		new Bot();
+	}
+	
+}
